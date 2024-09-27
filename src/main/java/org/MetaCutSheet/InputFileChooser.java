a20ef9b848c158954a2ff3c638b055c747b61417
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2024-09-26 22:37:13 -0700

    successfully updated code to jnaFileChooser for open and save methods.

--- src/main/java/org/MetaCutSheet/InputFileChooser.java
+++ src/main/java/org/MetaCutSheet/InputFileChooser.java
@@ -2,8 +2,6 @@ package org.MetaCutSheet;
 
 import jnafilechooser.api.JnaFileChooser;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,56 +13,12 @@ public class InputFileChooser {
 
     static String userPDFFileInput() {
 
-//        /// failure due to multi-thread attept or can not return result in lambda form...
-//
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle("Open Resource File");
-//        fileChooser.getExtensionFilters().
-//
-//                addAll(
-//                        new FileChooser.ExtensionFilter("PDF", "*.pdf"),
-//                        new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-//                        new FileChooser.ExtensionFilter("All Files", "*.*"));
-//        File file = fileChooser.showOpenDialog(null);
-//
-//        final String selectedFilePath = file.getAbsolutePath();
-//
-//        return selectedFilePath;
-//
-//
-//    }
-
-
-
-//            Platform.runLater(new Runnable() {
-//                @Override
-//                public void run() {
-//                    //attempt to use javafx filechooser (failed)
-//                    FileChooser fileChooser = new FileChooser();
-//                    fileChooser.setTitle("Open Resource File");
-//                    fileChooser.getExtensionFilters().
-//
-//                            addAll(
-//                                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-//                                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-////                    new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
-//                                    new FileChooser.ExtensionFilter("All Files", "*.*"));
-//                    File selectedFile = fileChooser.showOpenDialog(null);
-////            if (selectedFile != null) {
-//////                mainStage.display(selectedFile);
-////            }
-//                }
-//            });
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-
 //         repository added possible solution to customer request. has issue with opening file 3 deep
         String selectedFilePath = null;
 
         JnaFileChooser jnaFileChooser = new JnaFileChooser();
         jnaFileChooser.addFilter("PDF Files", "pdf");
-        jnaFileChooser.setTitle("Choose File to Convert");
+        jnaFileChooser.setTitle("Choose PDF File to Convert");
         String testFile2 = "C:\\Computer Programming Projects\\Olivers PDF project\\sample cut sheets";
         jnaFileChooser.setCurrentDirectory(testFile2);
 
@@ -78,11 +32,33 @@ public class InputFileChooser {
         }
 
         return selectedFilePath;
+    }
 
+    public static File[] multipleImageFileUserInput() throws IOException {
+
+
+        String selectedFilePath;
+
+        JnaFileChooser jnaFileChooser = new JnaFileChooser();
+        jnaFileChooser.addFilter("Image Files", "jpg", "png");
+        jnaFileChooser.setTitle("Choose Image File(s) to Convert");
+        jnaFileChooser.setMultiSelectionEnabled(true);
+        String testFile2 = "C:\\Computer Programming Projects\\Olivers PDF project\\sample cut sheets";
+        jnaFileChooser.setCurrentDirectory(testFile2);
+
+        if (jnaFileChooser.showOpenDialog(null)) {
+            File file = jnaFileChooser.getSelectedFile();
+            selectedFilePath = file.getAbsolutePath();
+
+            System.out.println("\n" + "Selected input file: " + selectedFilePath + "\n");
+        } else {
+            System.out.println("No file selected.");
         }
 
+        return jnaFileChooser.getSelectedFiles();
 
-////         using jchooser/swing styling
+
+
 //        try {
 //            for (UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
 //                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
@@ -92,77 +68,25 @@ public class InputFileChooser {
 //            throw new RuntimeException(e);
 //        }
 //
+//        // need to add muti-file option for images
 //
-//        // Create a file chooser
-//        JFileChooser jFileChooser = new JFileChooser();
+//        JFrame frame = new JFrame();
+//        JFileChooser chooser = new JFileChooser();
+//        chooser.setDialogTitle("Choose file(s) to Convert");
 //
-//        // Optionally, set the file chooser to select only files with a specific extension
-//        // need to figure out how to save last file path*********************
-//
-/////////////////////////////// For Test Mode
 //        File testFile = new File("C:\\Computer Programming Projects\\Olivers PDF project\\sample cut sheets");
-///////////////////////////////
+//        chooser.setCurrentDirectory(testFile);
+//        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "png");
+//        chooser.setFileFilter(filter);
+//        chooser.setMultiSelectionEnabled(true);
+//        chooser.showOpenDialog(frame);
 //
-//        FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Files", "pdf");
-//        jFileChooser.setFileFilter(filter);
-//        jFileChooser.setDialogTitle("Choose File to Convert");
 //
-//        ///////////////////////////// For Test Mode
-//        jFileChooser.setCurrentDirectory(testFile);
-//        ///////////////////////////////
-//
-//        // Show the file chooser dialog
-//        int result = jFileChooser.showOpenDialog(null);
-//
-//        //Get file path from user
-//        String selectedFilePath = null;
-//        // Check if a file was selected
-//        if (result == JFileChooser.APPROVE_OPTION) {
-//
-//            // Get the selected file
-//            File file = jFileChooser.getSelectedFile();
-//            selectedFilePath = file.getAbsolutePath();
-//
-//            System.out.println("\n" + "Selected input file: " + file.getAbsolutePath() + "\n");
-//        } else {
-//            System.out.println("No file selected.");
-//        }
-//
-//        return selectedFilePath;
-//    }
-
-
-
-    public static File[] multipleImageFileUserInput() throws IOException {
-
-        try {
-            for (UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        // need to add muti-file option for images
-
-        JFrame frame = new JFrame();
-        JFileChooser chooser = new JFileChooser();
-        chooser.setDialogTitle("Choose file(s) to Convert");
-
-        File testFile = new File("C:\\Computer Programming Projects\\Olivers PDF project\\sample cut sheets");
-        chooser.setCurrentDirectory(testFile);
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "png");
-        chooser.setFileFilter(filter);
-        chooser.setMultiSelectionEnabled(true);
-        chooser.showOpenDialog(frame);
-
-
-        //////////////////////
-        return chooser.getSelectedFiles();
-        }
-
-
+//        //////////////////////
+//        return chooser.getSelectedFiles();
     }
+
+
+}
 
 
--- src/main/java/org/MetaCutSheet/SaveFinalPDF.java
+++ src/main/java/org/MetaCutSheet/SaveFinalPDF.java
@@ -1,8 +1,8 @@
 package org.MetaCutSheet;
 
+import jnafilechooser.api.JnaFileChooser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,46 +10,76 @@ public class SaveFinalPDF {
 
     public static String saveFinalPDF(PDDocument final_cs) throws IOException {
 
-        // cleanup
-        // need to figure out how to save last file path*********************
-        //String selectedSaveFilePath = null;
-        JFileChooser saveFileChooser = new JFileChooser();
 
-////////////////////////////// For test mode
-        File firstSaveDir = new File("C:\\Users\\James\\Desktop\\temp");
-        saveFileChooser.setCurrentDirectory(firstSaveDir);
-//////////////////////////////
-
-        saveFileChooser.setDialogTitle("Choose Save Directory");
-
-        // Show the file chooser dialog
-        int userSaveDir = saveFileChooser.showSaveDialog(null);
-
-        //Get file path from user
         String selectedSaveFilePath = null;
 
-        // Check if a file was selected
-        if (userSaveDir == JFileChooser.APPROVE_OPTION) {
-            // Get the selected file
-            java.io.File file = saveFileChooser.getSelectedFile();
-            //System.out.println("user chosen file path and name: " + file + "\n");
+        JnaFileChooser jnaFileChooser = new JnaFileChooser();
+        jnaFileChooser.setTitle("Choose Save Directory");
+        jnaFileChooser.addFilter("PDF Documents (*.pdf)", "pdf");
+        String testFile2 = "C:\\\\Users\\\\James\\\\Desktop\\\\temp";
+        jnaFileChooser.setCurrentDirectory(testFile2);
 
-            if (file.getAbsolutePath().contains(".pdf")) {
-                selectedSaveFilePath = file.getAbsolutePath();
-            }else {
-                selectedSaveFilePath = file.getAbsolutePath() + ".pdf";
+        if (jnaFileChooser.showSaveDialog(null)) {
+            File saveFileDir = jnaFileChooser.getSelectedFile();
+
+            if (saveFileDir.getAbsolutePath().contains(".pdf")) {
+                selectedSaveFilePath = saveFileDir.getAbsolutePath();
+            } else {
+                selectedSaveFilePath = saveFileDir.getAbsolutePath() + ".pdf";
             }
 
-        } else {
-            System.out.println("No file selected." + "\n");
-        }
+            System.out.println("\n" + "Selected save file: " + selectedSaveFilePath + "\n");
 
-        //need to change save location and name
+        } else {
+            System.out.println("No file selected.");
+        }
         final_cs.save(selectedSaveFilePath);
 
         System.out.println("Successfully saved to : " + selectedSaveFilePath + "\n");
 
         final_cs.close();
+
+
+        // cleanup
+        // need to figure out how to save last file path*********************
+        //String selectedSaveFilePath = null;
+//        JFileChooser saveFileChooser = new JFileChooser();
+//
+//////////////////////////////// For test mode
+//        File firstSaveDir = new File("C:\\Users\\James\\Desktop\\temp");
+//        saveFileChooser.setCurrentDirectory(firstSaveDir);
+////////////////////////////////
+//
+//        saveFileChooser.setDialogTitle("Choose Save Directory");
+//
+//        // Show the file chooser dialog
+//        int userSaveDir = saveFileChooser.showSaveDialog(null);
+//
+//        //Get file path from user
+//        String selectedSaveFilePath = null;
+//
+//        // Check if a file was selected
+//        if (userSaveDir == JFileChooser.APPROVE_OPTION) {
+//            // Get the selected file
+//            java.io.File file = saveFileChooser.getSelectedFile();
+//            //System.out.println("user chosen file path and name: " + file + "\n");
+//
+//            if (file.getAbsolutePath().contains(".pdf")) {
+//                selectedSaveFilePath = file.getAbsolutePath();
+//            }else {
+//                selectedSaveFilePath = file.getAbsolutePath() + ".pdf";
+//            }
+//
+//        } else {
+//            System.out.println("No file selected." + "\n");
+//        }
+//
+//        //need to change save location and name
+//        final_cs.save(selectedSaveFilePath);
+//
+//        System.out.println("Successfully saved to : " + selectedSaveFilePath + "\n");
+//
+//        final_cs.close();
 
         return selectedSaveFilePath;
 
