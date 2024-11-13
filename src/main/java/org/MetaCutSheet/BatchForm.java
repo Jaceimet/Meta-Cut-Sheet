d109bf07870d6d698b44e6dbac747edc7a130461
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2024-11-12 22:54:12 -0800

    cleaned up code, need to connect excel form and final form

--- src/main/java/org/MetaCutSheet/BatchForm.java
+++ src/main/java/org/MetaCutSheet/BatchForm.java
@@ -18,47 +18,35 @@ import java.util.Map;
 
 public class BatchForm {
 
+    static String type;
+    static String manufacturer;
+    static String modelNumber;
+    static String partNumber;
+    static String description;
+    static String wattage;
+    static String voltage;
+    static String control;
+    static String dimmable;
+    static String date;
+    static String revision;
+    static String approvedBy;
+    static String ld;
+    static String designFirm;
+    static String projectName;
+    static String projectLocation;
+    static String notes;
+
     public static PDDocument batchForm(PDDocument existingDocument){
 
 //https://www.youtube.com/watch?v=ipjl49Hgsg8&list=PLUDwpEzHYYLsN1kpIjOyYW6j_GLgOyA07
         //https://www.youtube.com/watch?v=JAyJSffgm7c
 
+        //hashmap data processing
+        //https://www.youtube.com/watch?v=rJ3-94VjWMg
+
 
         String excelFilePath = "src/main/resources/Cut Sheet Express excel.xlsx";
 
-//        InputStream url = FormSelector.class.getResourceAsStream(excelFilePath);
-//        existingDocument = Loader.loadPDF(PdfImageCreator.class.getResourceAsStream(excelFilePath).readAllBytes());
-
-
-//        try {
-//
-//            System.out.println(excelFilePath);
-//
-//            FileInputStream inputStream = new FileInputStream(excelFilePath);
-//
-//            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-//
-//            XSSFSheet sheet = workbook.getSheetAt(0); //XSSFSheet sheet = workbook.getSheet("sheet1")
-//
-//            int rowCount = sheet.getLastRowNum();
-//            System.out.println("row count : " + rowCount);
-
-
-//            for (int i =0; i <= rowCount; i++) {
-//                    int columnCount = sheet.getRow(i).getLastCellNum();
-//                System.out.println("Cell count: " + columnCount);
-//
-//                    for (int j = 0; j < columnCount; j++) {
-//                        System.out.println(sheet.getRow(i).getCell(j).toString());
-//                    }
-//                    System.out.println();
-//
-////                }else {
-////                    System.out.println("no more row to process");
-////                }
-//
-//
-//            }
 
             try {
 
@@ -77,12 +65,14 @@ public class BatchForm {
                     XSSFRow row = sheet.getRow(i);
 
                     //fails here, need if statement?
-//                if (row != null) {
                     int columnCount = row.getPhysicalNumberOfCells();
                     System.out.println("Cell count: " + columnCount);
 
                     for (int j = 0; j < columnCount; j++) {
                         XSSFCell cell = row.getCell(j);
+
+
+
 //                    String cellValue = getCellValue(cell);
                         switch (cell.getCellType()) {
                             case STRING:
@@ -103,85 +93,36 @@ public class BatchForm {
 
                     }
                     System.out.println();
-
-//                }else {
-//                    System.out.println("no more row to process");
-//                }
-
-
                 }
-
-
-
-//            int rows = sheet.getLastRowNum();
-//            System.out.println("Number of rows: " + rows);
-//            int columns = sheet.getRow(1).getLastCellNum();
-//            System.out.println("Number of Columns: " + columns);
-//            for(int r = 0; r <= rows; r++){
-//
-//                XSSFRow row = sheet.getRow(r);
-//
-//                // error row is null?
-//                for(int c = 0; c < columns; c++) {
-//
-//                    //fails here need fix
-//                    XSSFCell cell = row.getCell(c);
-//
-//
-//                        switch (cell.getCellType()) {
-//                            case STRING:
-//                                System.out.println(cell.getStringCellValue());
-//                                break;
-//                            case NUMERIC:
-//                                System.out.println(cell.getNumericCellValue());
-//                                break;
-//                            case BOOLEAN:
-//                                System.out.println(cell.getBooleanCellValue());
-//                                break;
-//                            case _NONE, ERROR, BLANK:
-//                                System.out.println("blank cell possible error");
-//                                break;
-//                            default:
-//                                throw new IllegalStateException("Unexpected value: " + cell.getCellType());
-//                        }
-//
-//                    }
-//                    System.out.println();
-//
-//            }
-
-
         } catch (IOException e) {
-            System.err.println("Ran out of rows " + e.getMessage());
+            System.err.println(e.getMessage());
         }
-
-
 
 
         Map<String, String> fields = new HashMap<>();
 
-        fields.put("type", "a");
-        fields.put("manufacturer", "b");
+        fields.put("type", type);
+        fields.put("manufacturer", manufacturer);
         // incorrect format?
-        fields.put("modelNumber", "3234");
-        fields.put("partNumber", "d");
-        fields.put("description", "e");
+        fields.put("modelNumber", modelNumber);
+        fields.put("partNumber", partNumber);
+        fields.put("description", description);
         // incorrect format?
-        fields.put("wattage", ("f").formatted());
-        fields.put("voltage", "g");
-        fields.put("control", "h");
-        fields.put("dimmable", "i");
+        fields.put("wattage", wattage);
+        fields.put("voltage", voltage);
+        fields.put("control", control);
+        fields.put("dimmable", dimmable);
         // // incorrect format?
         LocalDate today = LocalDate.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        fields.put("date", dtf.format(today).toString()); //("123").formatted()
-        fields.put("revision", "k");
-        fields.put("approvedBy", "l");
-        fields.put("ld", "m");
-        fields.put("designFirm", "n");
-        fields.put("projectName", "o");
-        fields.put("projectLocation", "p");
-        fields.put("notes", "q");
+        fields.put("date", dtf.format(today)); //("123").formatted()
+        fields.put("revision", revision);
+        fields.put("approvedBy", approvedBy);
+        fields.put("ld", ld);
+        fields.put("designFirm", designFirm);
+        fields.put("projectName", projectName);
+        fields.put("projectLocation", projectLocation);
+        fields.put("notes", notes);
         PDDocumentCatalog docCatalog = existingDocument.getDocumentCatalog();
         PDAcroForm acroForm = docCatalog.getAcroForm();
         acroForm.setCacheFields(true);
